95b867f10fdec7dc5524af4b0f0510eea85828c2
Justin Wesley <>
2024-08-15 20:55:57 -0500

    Refactor to use @TestTemplate extension

--- src/main/kotlin/com/wesleyhome/test/jupiter/annotations/GeneratedParametersTestExtension.kt
+++ src/main/kotlin/com/wesleyhome/test/jupiter/annotations/GeneratedParametersTestExtension.kt
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace.create
 import org.junit.jupiter.api.extension.TestTemplateInvocationContext
 import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider
+import org.junit.jupiter.params.ParameterizedTest
 import org.junit.platform.commons.util.AnnotationUtils.isAnnotated
+import org.junit.platform.commons.util.Preconditions
 import java.lang.reflect.Method
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.stream.Stream
@@ -15,6 +17,10 @@ class GeneratedParametersTestExtension : TestTemplateInvocationContextProvider {
         return if (!context.testMethod.isPresent) false else {
             val method = context.testMethod.get()
             if (isAnnotated(method, GeneratedParametersTest::class.java)) {
+                Preconditions.condition(
+                    !isAnnotated(method, ParameterizedTest::class.java),
+                    "Test annotated with @GeneratedParametersTest cannot be annotated with @ParameterizedTest"
+                )
                 getStore(context).put(METHOD_CONTEXT_KEY, GeneratedParametersTestMethodContext(context))
                 true
             } else false
@@ -29,10 +35,12 @@ class GeneratedParametersTestExtension : TestTemplateInvocationContextProvider {
         val methodContext = getStore(extensionContext).get(METHOD_CONTEXT_KEY, GeneratedParametersTestMethodContext::class.java)
         println(methodContext)
         val arguments = methodContext.generator.arguments()
-        val indexCounter = AtomicInteger(1)
-        val stream: Stream<TestTemplateInvocationContext> = StreamSupport.stream(arguments.spliterator(), false)
-            .map { GeneratedParametersTestInvocationContext(it.get().toList()) }
-        return stream
+        val invocationCount = AtomicInteger(0)
+        return StreamSupport.stream(arguments.spliterator(), false)
+            .map {
+                invocationCount.incrementAndGet()
+                GeneratedParametersTestInvocationContext(it.get().toList())
+            }
     }
 
     private fun getStore(context: ExtensionContext): ExtensionContext.Store {
