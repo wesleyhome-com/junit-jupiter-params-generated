b8403f408ac8d3ba9895bb8e6e247864bfe56860
Justin Wesley <>
2024-12-05 18:55:28 -0600

    More work on releasing

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,52 +61,10 @@ nexusPublishing {
     }
 }
 
-task("release") {
-    doLast {
-        val version = versionString.removeSuffix("-SNAPSHOT")
-        val versionParts = version.split(".")
-        val lastVersion = versionParts.last()
-        val nextVersion = providers.environmentVariable("next-version")
-            .orElse(
-                versionParts.subList(0, versionParts.size - 1)
-                    .plus((lastVersion.toInt() + 1).toString())
-                    .joinToString(separator = ".", postfix = "-SNAPSHOT")
-            ).get()
-        releaseVersion(version)
-        releaseVersion(nextVersion)
-        exec {
-            commandLine("cmd", "/c", "git", "push", "origin", "--tags")
-        }
-
-    }
-}
-fun releaseVersion(version: String) {
-    val propsFile = File("gradle.properties")
-    propsFile.bufferedWriter().use {
-        it.write("version=$version%n".format())
-    }
-    val isSnapshot = version.endsWith("-SNAPSHOT")
-    val action = if (isSnapshot) {
-        "Creating new snapshot"
-    } else {
-        "Releasing Version"
-    }
-    exec {
-        commandLine("cmd", "/c", "git", "add", "gradle.properties")
-    }
-    exec {
-        commandLine("cmd", "/c", "git", "commit", "-m", """"$action $version"""")
-    }
-    if (!isSnapshot) {
-        exec {
-            commandLine("cmd", "/c", "git", "tag", """"junit-jupiter-params-generated-$version"""")
-        }
-    }
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
+task<Copy>("updateReadme") {
+    doNotTrackState("This task does not support up-to-date checks.")
+    from("README.template.md")
+    into(".")
+    rename { "README.md" }
+    expand(mapOf("version" to project.version))
 }
