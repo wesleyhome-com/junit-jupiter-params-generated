9bf9c58145e65f74acd46a43f8442239985055cc
Justin Wesley <>
2024-12-30 18:56:06 -0600

    Fix gradle imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-import org.gradle.kotlin.dsl.assign
-import org.jetbrains.dokka.DokkaGenerator
-import org.jetbrains.dokka.gradle.DokkaExtension
-import org.jetbrains.dokka.gradle.DokkaTask
-
 plugins {
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     id("net.researchgate.release") version "3.1.0"
